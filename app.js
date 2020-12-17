6a8de61f29bae098452cbcfcf3033ad8fe8bb311
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-12-16 23:24:44 -0800

    Instantiated new roles and pushed results into empty array called team

--- app.js
+++ app.js
@@ -61,7 +61,12 @@ function teamMembers() {
                         name: "github"
                     }
 
-                ]);
+                ]).then(({name, id, email, github}) => {
+                    let engineer = new Engineer(name, id, email, github);
+                    team.push(engineer);
+                    addMember();
+
+                })
             } else if (response.role === "Intern") {
                 inquirer.prompt([
 
@@ -89,37 +94,46 @@ function teamMembers() {
                         name: "school"
                     }
 
-                ]);
-            } else {
-                inquirer.prompt([
+                ]).then(({name, id, email, school}) => {
+                    let intern = new Intern(name, id, email, school);
+                    team.push(intern);
+                    addMember();
 
-                    {
+                }) } else {
+                    inquirer.prompt([
 
-                        type: "input",
-                        message: "Enter team member's name.",
-                        name: "name"
-                    },
+                        {
+
+                            type: "input",
+                            message: "Enter team member's name.",
+                            name: "name"
+                        },
 
 
-                    {
-                        type: "input",
-                        message: "Enter team member's id.",
-                        name: "id"
-                    },
-                    {
-                        type: "input",
-                        message: "Enter team member's email.",
-                        name: "email"
-                    },
-                    {
-                        type: "input",
-                        message: "Please enter office number",
-                        name: "officeNumber"
-                    }
+                        {
+                            type: "input",
+                            message: "Enter team member's id.",
+                            name: "id"
+                        },
+                        {
+                            type: "input",
+                            message: "Enter team member's email.",
+                            name: "email"
+                        },
+                        {
+                            type: "input",
+                            message: "Please enter office number",
+                            name: "officeNumber"
+                        }
 
-                ]);
+                    ]).then(({name, id, email, officeNumber}) => {
+                        let manager = new Manager(name, id, email, officeNumber);
+                        team.push(manager);
+                        addMember();
 
-            }
+                    })
+                }
+
         })
 
     } catch (err) {
@@ -130,7 +144,6 @@ function teamMembers() {
 }
 
 
-
 function addMember() {
     inquirer.prompt([
         {
@@ -139,9 +152,14 @@ function addMember() {
             name: "member"
         }
     ]).then(function (confirm) {
-        confirm ? teamMembers() : render();
+        console.log(confirm);
+        confirm.member ? teamMembers() : loadPage();
     })
 }
+
+
+
+
 
 
 
@@ -149,22 +167,4 @@ function addMember() {
 teamMembers();
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
